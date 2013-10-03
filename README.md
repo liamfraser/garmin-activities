42742bfa33a7fba39059217d43aeae36bc063f09
Liam Fraser <liam@liamfraser.co.uk>
2013-10-03 15:28:20 +0000

    Bunch of bugfixes and functionality added

--- README.md
+++ README.md
@@ -26,6 +26,10 @@ A simple Python module that allows you to log into Garmin Connect and get activi
      |  
      |  get_latest(self)
      |      Return an instance of the activity class for the latest activity
+     |  
+     |  get_week(self, week=None)
+     |      Return a list of instances of the activity class for the week
+     |      (Monday - Sunday). Optionally takes the week of the year to get
     
     class activity
      |  A class that represents a Garmin Connect activity
@@ -35,7 +39,18 @@ A simple Python module that allows you to log into Garmin Connect and get activi
      |  __init__(self, json)
      |  
      |  ----------------------------------------------------------------------
+     |  Static methods defined here:
+     |  
+     |  pace_calculator(duration_seconds, distance)
+     |      Returns the pace as a datetime.datetime instance. Takes the duration
+     |      in seconds and distance as arguments
+     |  
+     |  ----------------------------------------------------------------------
      |  Data descriptors defined here:
+     |  
+     |  datetime
+     |      Returns the date and time that the run started as an instance of
+     |      datetime.datetime
      |  
      |  distance
      |      Returns the distance of the activity as a float
@@ -59,6 +74,13 @@ A simple Python module that allows you to log into Garmin Connect and get activi
      |  pace
      |      Returns the pace as a datetime.datetime instance
      |  
+     |  pace_unit
+     |      Returns an abbreviation of the unit in pace form
+     |  
+     |  short_unit
+     |      Returns an abbreviation of the unit
+     |  
      |  unit
      |      Returns the unit of measurement for the activity. Either 'mile' or
      |      'kilomiter'
+
--- garmin.py
+++ garmin.py
@@ -26,7 +26,9 @@
 import mechanize
 import cookielib
 import simplejson
-from datetime import time, datetime, timedelta
+import re
+import time
+import datetime
 from decimal import *
 
 class activity:
@@ -57,7 +59,7 @@ class activity:
         hours = int(split[0])
         minutes = int(split[1])
         seconds = int(split[2])
-        return time(hour = hours, minute = minutes, second = seconds)
+        return datetime.time(hour = hours, minute = minutes, second = seconds)
 
     @property
     def duration_seconds(self):
@@ -90,17 +92,75 @@ class activity:
         return self.json['sumDistance']['uom']
 
     @property
+    def short_unit(self):
+        """Returns an abbreviation of the unit"""
+
+        unit_map = {"mile" : "Mi",
+                    "kilomiter" : "Km"}
+
+        return unit_map[self.unit]
+    
+    @property
+    def pace_unit(self):
+        """Returns an abbreviation of the unit in pace form"""
+
+        unit_map = {"mile" : "Min/Mi",
+                    "kilomiter" : "Min/Km"}
+
+        return unit_map[self.unit]
+
+
+    @property
     def pace(self):
         """Returns the pace as a datetime.datetime instance"""
 
-        seconds_per_distance = self.duration_seconds / self.distance
+        return self.pace_calculator(self.duration_seconds, self.distance)
+
+    @staticmethod
+    def pace_calculator(duration_seconds, distance):
+        """Returns the pace as a datetime.datetime instance. Takes the duration
+        in seconds and distance as arguments"""
+
+        seconds_per_distance = float(duration_seconds / distance)
 
         # Convert the seconds back to minutes and seconds
-        pace_td = timedelta(seconds = seconds_per_distance)
-        pace_d = datetime(1,1,1) + pace_td
+        pace_td = datetime.timedelta(seconds = seconds_per_distance)
+        pace_d = datetime.datetime(1,1,1) + pace_td
 
         return pace_d
+
+    @property
+    def datetime(self):
+        """Returns the date and time that the run started as an instance of
+        datetime.datetime"""
+
+        start_time = self.json['beginTimestamp']['display']
+        # We need to convert a string in the form:
+        # Wed, Sep 25, 2013 19:09
+        # To an instance of datetime
+
+        t_match = re.match('(\w+), (\w+) (\d+), (\d+) (\d+):(\d+)',
+                           start_time)
+        t_day_str = t_match.group(1)
+        t_month_str = t_match.group(2)
+        t_day_of_month = t_match.group(3)
+        # For year, we only want the last 2 digits
+        t_year_str = t_match.group(4)[-2:]
+        t_hours = t_match.group(5)
+        t_minutes = t_match.group(6)
         
+        # Convert this to a string that can be parsed with strptime
+        t_str = "{0} {1} {2} {3} {4}".format(t_day_of_month,
+                                             t_month_str,
+                                             t_year_str,
+                                             t_hours,
+                                             t_minutes)
+        start_time = time.strptime(t_str, "%d %b %y %H %M")
+
+        # Now turn the struct_time object into a datetime object
+        start_time = datetime.datetime.fromtimestamp(time.mktime(start_time))
+
+        return start_time
 
 class activities:
     """A simple Python class that allows you to log into Garmin Connect and get
@@ -178,18 +238,19 @@ class activities:
         total = 0
 
         while i < total or total == 0:
-            json = self._api_request(i)
+            json = self._get_activity_page(i)
             
             # Find out how many activities we have on Garmin Connect            
             try:
-                total = json['results']['search']['totalFound']
-            except KeyError:
+                total = json['search']['totalFound']
+            except KeyError as error:
                 # Bad data from API
+                print "Key Error: "   + error.message
                 return []
 
             # Add the number of activities we got to i
             try:
-                i += len(json['results']['activities'])
+                i += len(json['activities'])
             except KeyError:
                 # We have no more activities
                 break
@@ -200,8 +261,37 @@ class activities:
         # Turn the json into a collection of activities we can return
         activities = []
 
-        for j in jsons:
-            for a in json['results']['activities']:
+        for json in jsons:
+            for a in json['activities']:
                 activities.append(activity(a))
 
         return activities
+
+    def get_week(self, week = None):
+        """Return a list of instances of the activity class for the week
+        (Monday - Sunday). Optionally takes the week of the year to get"""
+
+        # We're going to be lazy about this and filter the results from the
+        # the get_all function because we won't be making api requests
+        # frequently
+
+        all_activities = self.get_all()
+        week_activities = []
+
+        # Get the week number of this week
+        week_num = None
+        if week == None:
+            now = datetime.datetime.now()
+            year, week_now, day_of_week = now.isocalendar()
+            week_num = week_now
+        else:
+            week_num = week
+
+        # Loop through each activity and add it to the collection if it's in
+        # this week
+        for a in all_activities:
+            a_year, a_week, a_day_of_week = a.datetime.isocalendar()
+            if a_week == week_num:
+                week_activities.append(a)
+        
+        return week_activities
